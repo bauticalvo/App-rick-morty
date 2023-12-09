347c2cf0e3a44e0581ff3c839dd75ce72c0bc6b9
bauticalvo <bito.bc@gmail.com>
2023-12-09 16:05:35 -0300

    Done

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -46,7 +46,7 @@ function App() {
    useEffect(() => {
       // este es el correcto
       //!access && navigate('/'); 
-      !access && navigate('/home');
+      !access && navigate('/');
    }, [access]);
 
    function logout(){
